664bd149f8231eeabd0f787449f4bfd670403d2c
vivekmahato26 <vivekmahato26@gmail.com>
2022-11-16 17:53:38 +0530

    class 16-11-22

---
+++ src/components/authPage.js
@@ -0,0 +1,81 @@
+import axios from "axios"
+import { useState,useRef } from "react"
+import { baseUrl } from "../constants"
+import "../styles/authPage.css"
+
+const AuthPage = () => {
+    const [registerData,setRegisterData] = useState()
+    const [registerError,setRegisterError] = useState()
+    const registerRef = useRef();
+    const handleRegister = async(event) => {
+        event.preventDefault();
+        const regForm = registerRef.current;
+        const body = {};
+        body.name = regForm.name.value;
+        body.email = regForm.email.value;
+        body.phone = regForm.phone.value;
+        body.password = regForm.password.value;
+        body.access = regForm.access.value;
+
+        try {
+            
+            const data =  await axios.post(baseUrl+"users/register",body);
+            console.log(data);
+            setRegisterData(data);
+            if(data.data._id) {
+                setRegisterError(null);
+            } else {
+                setRegisterError(data.data);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+        
+    }
+    return (
+        <div className="authContainer">
+            {/* <form className="loginForm">
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" />
+                </div>
+                <button type="submit">Login</button>
+            </form> */}
+            <form ref={registerRef} className="registerForm" onSubmit={(event) => handleRegister(event)}>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="name" name="name"  />
+                </div>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input className={registerError ? "error": ""} type="email" name="email" />
+                    {registerError && <span>{registerError}</span>}
+                </div>
+                <div>
+                    <label htmlFor="phone">Phone</label>
+                    <input type="phone" name="phone" />
+                </div>
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" />
+                </div>
+                <div>
+                    <label htmlFor="access">Access</label>
+                    <select name="access">
+                        <option value="Admin">Admin</option>
+                        <option value="Viewer">Viewer</option>
+                        <option value="Editor">Editor</option>
+                    </select>
+                </div>
+                <button type="submit">Register</button>
+            </form>
+        </div>
+    )
+}
+
+export default AuthPage;
